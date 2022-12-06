8f3fac2397d5d8ad41e0b736859307a857b96fbb
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2022-12-06 18:47:00 -0500

    Add files via upload

---
+++ Descriptive_Example.R
@@ -0,0 +1,227 @@
+#Script to create the Dummy_Education_Plot_Example.png
+#These were the type of plots were generated from the ABCD Descriptive script
+pacman::p_load(dplyr, ggplot2, tidyr)
+data = readxl::read_xlsx("~/data.xlsx")
+data = data[2:nrow(data),]
+data = data.frame(ID = 1:nrow(data), data)
+
+
+Q6_4_Grouped = c()
+for(i in data$Q6_4){
+  if(i %in% c(0, NA)){
+    convert = NA
+  }
+  else{
+    convert = 'Group_W'
+  }
+  Q6_4_Grouped = c(Q6_4_Grouped ,convert)
+}
+
+data$Q6_4_Grouped  =Q6_4_Grouped 
+
+
+#############################
+
+#Grouped data
+#Survey was multiple choice and multiple choice answers were seperated by commas
+Q2 = separate_rows(data, "Q2_Grouped", sep = ",")
+summary(factor(Q2$Q2_Grouped, exclude = NA))
+
+
+Group_R = which(Q2$Q2_Grouped == "Group_R")
+Group_R = Q2[Group_R,]
+Group_R$Group = Group_R$Q2_Grouped
+
+
+Group_T = which(Q2$Q2_Grouped =="Group_T")
+Group_T = Q2[Group_T,]
+Group_T$Group = Group_T$Q2_Grouped
+
+  
+Group_W= which(data$Q6_4_Grouped == "Group_W")
+Group_W = data[c(Group_W),]
+Group_W$Group = Group_W$Q6_4_Grouped
+
+
+Q4 = separate_rows(data, "Q4_Grouped", sep = ",")
+
+summary(factor(Q4$Q4_Grouped))
+
+Group_C = which(Q4$Q4_Grouped =="Group_C")
+Group_C = Q4[Group_C,]
+Group_C$Group = Group_C$Q4_Grouped
+
+Group_V = which(Q4$Q4_Grouped =="Group_V")
+Group_V = Q4[Group_V,]
+Group_V$Group = Group_V$Q4_Grouped
+
+summary(factor(data$Q30_Grouped))
+
+Group_M = which(data$Q30_Grouped=="Group_M")
+Group_M = data[Group_M,]
+Group_M$Group = Group_M$Q30_Grouped
+
+combined_data = rbind(Group_R,Group_T,Group_V,Group_W,Group_C,
+                      Group_M)
+
+combined_data = data.frame(combined_data[c("ID","Group", "Q8_Simplified")])
+summary(factor(combined_data$Group))
+combined_data$Group = factor(combined_data$Group, levels=c("Group_V",
+                                                           "Group_C",
+                                                           "Group_M",
+                                                           "Group_W",
+                                                           "Group_R",
+                                                           "Group_T"
+                                                           ))
+
+
+summary(factor(combined_data$Group))
+colnames(combined_data) = c("ID", "Group","Education")
+
+combined_data$ID = 1:nrow(combined_data)
+
+#Generate random data
+
+groups = combined_data["Group"]; groups = as.character(groups$Group); groups = unique(groups)
+groups = sample(groups, nrow(combined_data), replace = T)
+
+combined_data["Group"] = groups
+
+education_groups = combined_data["Education"]; education_groups = unique(education_groups$Education)
+
+
+for(group in groups){
+  prob = 0
+  #While loop to create probabilities for each education variable
+  #Purpose is to replicate a sample that where the probabilities do not exceed 100 and are not less than 80
+  #Creates a unique distribution for each of the groups for plotting
+  while(!(sum(prob) < 100 & sum(prob) > 80 )){
+    #Max is 100/length(education_groups) so that even in the incredibly rare chance that all variables qual the max
+    #The max will not exceed 100
+    prob = runif(length(education_groups), min = 0, max = 100/length(education_groups))
+  }
+  #Convert to percent
+  prob = prob/100
+  combined_data[combined_data$Group  == group,"Education"] = sample(education_groups, nrow(combined_data[combined_data$Group  == group,]), replace = T, prob = prob)
+  
+}
+
+binary_data = data.frame("ID"= combined_data$ID, "Group"= combined_data$`Group`, "High_school_or_equivalent"= rep(NA, nrow(combined_data)), "Some_college" = rep(NA, nrow(combined_data)), "Associates_Degree" = rep(NA, nrow(combined_data)),"Bachelors_Degree" = rep(NA, nrow(combined_data)),
+                         "Masters_Degree" = rep(NA, nrow(combined_data)), "PhD_ProfessionalDegree" = rep(NA, nrow(combined_data)), "Other_Ed" = rep(NA, nrow(combined_data)))
+
+
+
+summary(factor(combined_data$Education, exclude = NA))
+
+
+binary_data[c(which(combined_data$Education== "High School")), "High_school_or_equivalent"] = "Yes"
+binary_data[c(which(combined_data$Education == "Some College")), "Some_college"] = "Yes"
+binary_data[c(which(combined_data$Education == "Associate’s")), "Associates_Degree"] = "Yes"
+binary_data[c(which(combined_data$Education == "Bachelor’s")), "Bachelors_Degree"] = "Yes"
+binary_data[c(which(combined_data$Education == "Master’s")), "Masters_Degree" ] = "Yes"
+binary_data[c(which(combined_data$Education == "Ph.D./M.D.")), "PhD_ProfessionalDegree"] = "Yes"
+binary_data[c(which(combined_data$Education == "Other")), "Other_Ed"] = "Yes"
+
+
+
+binary_data[is.na(binary_data)] <- "No"
+
+
+
+
+#Using tidyr to summarize information by group
+Education = binary_data %>% 
+  group_by(Group) %>%
+  summarise("High School" = sum(High_school_or_equivalent=="Yes"),
+            "Some College" = sum(Some_college=="Yes"),
+            "Associate’s" = sum(Associates_Degree=="Yes"),
+            "Bachelor’s" = sum(Bachelors_Degree=="Yes"),
+            "Master’s" = sum(Masters_Degree=="Yes"),
+            "Ph.D./M.D." = sum(PhD_ProfessionalDegree=="Yes"),
+            "Other"= sum(Other_Ed=="Yes"))
+
+
+
+Education =  Education%>% 
+  pivot_longer(cols = -Group, names_to = "Variable", values_to = "Percentage")
+Education = data.frame(Education)
+
+
+#Convert counts to percentages
+combined_data[which(combined_data$Group == groups[1]),]
+n = summary(factor(combined_data[["Group"]], exclude = NA))
+
+
+summary(factor(Education[["Group"]], exclude = NA))
+
+Education[which(Education$Group=="Group_C"), "Percentage"] = round(
+  Education[which(Education$Group=="Group_C"), "Percentage"]/n[[1]]*100,
+  0
+)
+
+Education[which(Education$Group=="Group_M"), "Percentage"] = round(
+  Education[which(Education$Group=="Group_M"), "Percentage"]/n[[2]]*100,
+  0
+)
+
+Education[which(Education$Group=="Group_R"), "Percentage"] = round(
+  Education[which(Education$Group=="Group_R"), "Percentage"]/n[[3]]*100,
+  0
+)
+
+Education[which(Education$Group=="Group_T"), "Percentage"] = round(
+  Education[which(Education$Group=="Group_T"), "Percentage"]/n[[4]]*100,
+  0
+)
+
+Education[which(Education$Group=="Group_V"), "Percentage"] = round(
+  Education[which(Education$Group=="Group_V"), "Percentage"]/n[[5]]*100,
+  0
+)
+
+
+Education[which(Education$Group=="Group_W"), "Percentage"] = round(
+  Education[which(Education$Group=="Group_W"), "Percentage"]/n[[6]]*100,
+  0
+)
+
+
+
+
+
+
+Education$Variable = factor(Education$Variable, levels = c(
+  "Other",
+  "Ph.D./M.D.",
+  "Master’s",
+  "Bachelor’s",
+  "Associate’s",
+  "Some College",
+  "High School"
+  
+))
+
+
+
+new_colors = c()
+
+colors = c("#6639B5","#9B25AE", "#1F95F1", "#009586", "#4AAE4F", "#FEC006")
+#Length of color vector needs to equal length of rows
+#Each group needs its own unique color
+for(x in colors){
+  new_colors = c(new_colors, rep(x,7))
+}
+
+
+
+ggsave(filename = "Dummy_Education_Plot_Example.png",ggplot(Education, aes(Percentage, Variable, label = paste0(Percentage, "%"))) + 
+         geom_segment(aes(x = 0, y = Variable, xend = Percentage, yend = Variable), show.legend = FALSE, color = new_colors) + theme_bw() + facet_wrap(~Group, ncol = 3) + 
+         theme(plot.title = element_text(hjust = 0.5),text = element_text(size = 10), panel.grid.major = element_blank(),  axis.title.y=element_blank(), axis.title.x=element_blank(), axis.text.x = element_text(size=8), axis.text.y = element_text(size=8),
+               strip.background=element_rect(colour="black",
+                                             fill="grey95"))+ geom_point(size = 4, show.legend = FALSE, color = new_colors) +
+         expand_limits(x=c(0,100)) + geom_text(color = "white", size = 1.5), width = 7, height = 3, dpi = 300, units = "in", device='png')
+
+for(group in unique(groups)){
+ x = sum(Education[Education$Group  == group,"Percentage"])
+  print(x)
+}
